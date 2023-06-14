15adf6cfc7a4e33374738e22f316af64a91d3232
kmmaniez <dzlabsid@gmail.com>
2023-06-14 21:36:59 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,89 +1,35 @@
 @extends('layouts.master')
 
 @section('konten')
+    <div class="container-fluid">
 
-        <div class="container-fluid">
-
-            <!-- Page Heading -->
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">{{ Auth::user()->name ?? NULL }}</h1>
-            </div>
-            <!-- Content Row -->
-            {{-- <div class="row">
-                
-                <!-- Categories -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-bottom-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Categoriess</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $categories_count ?? NULL }}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-list fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Posts -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-bottom-success shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Posts</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $posts_count ?? NULL }}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!-- Products -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-bottom-success shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Products</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $products_count ?? NULL }}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-                <!-- User -->
-                <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card border-bottom-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Users</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users_count ?? NULL }}</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-user fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">{{ Auth::user()->name ?? null }}</h1>
         </div>
-    
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Categories -->
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-bottom-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-lg  font-weight-bold text-primary text-uppercase mb-1">Pilih SOpir</div>
+                                <div>
+                                    <select class="form-control" name="" id="">
+                                        <option value="">Farid</option>
+                                        <option value="">Eak</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- muncul paket sesuai paket yg dipilih --}}
+        </div>
+
+    </div>
 @endsection
