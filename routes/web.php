94e0eb0a01af1ba1f1a16fa738d0a53def26bc1d
kmmaniez <dzlabsid@gmail.com>
2023-06-21 08:35:32 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KendaraanController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\PaketController;
+use App\Http\Controllers\PengirimanController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SopirController;
 use Illuminate\Support\Facades\Route;
@@ -19,25 +20,55 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.dashboard');
+    return view('dashboard');
 })->middleware('auth');
 
 Route::get('/dashboard', function () {
-    return view('admin.dashboard');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+// Route::name('admin.')->prefix('admin')->group(function () {
+//     Route::get('/users', function () {
+//         // Route assigned name "admin.users"...
+//     })->name('users');
+// });
 
-Route::get('/laporan',[LaporanController::class]);
+// Route::name('admin.')->prefix('admin')->group(function () {
+
+
+
+    Route::prefix('laporan')->group(function () {
+        Route::get('/sopir', function () {
+            return view('admin.laporan.laporan-sopir', ['title_page' => 'Laporan Sopir']);
+        })->name('laporan.sopir');
+
+        Route::get('/paket', function () {
+            return view('admin.laporan.laporan-paket', ['title_page' => 'Laporan Paket']);
+        })->name('laporan.paket');;
+
+        Route::get('/tracking', function () {
+            return view('admin.laporan.laporan-tracking', ['title_page' => 'Laporan Tracking']);
+        })->name('laporan.tracking');;
+    });
+// });
+
 Route::get('/tracking', function () {
     return view('admin.tracking.create');
 });
 Route::resource('kendaraan', KendaraanController::class);
-// Route::resource('paket', PaketController::class)->except('show');
+
+Route::get('/checkpoint', function () {
+    return view('admin.checkpoint.index');
+});
+
+Route::controller(PengirimanController::class)->group(function () {
+    Route::get('/pengiriman', 'index');
+})->name('pengiriman');
 
 Route::controller(PaketController::class)->prefix('paket')->group(function(){
     Route::get('/', 'index')->name('paket.index');
@@ -58,20 +89,6 @@ Route::prefix('users')->group(function () {
     Route::resource('sopir', SopirController::class)->except('show');
 });
 
-Route::controller(LaporanController::class)->prefix('laporan')->group(function(){
-
-    Route::get('/', function () {
-        return view('admin.laporan.index');
-    });
-
-    Route::get('supir','LaporanSupir');
-    Route::get('kendaraan', function(){
-       return view('admin.kendaraan.create');
-    });
-    Route::get('tracking', function(){
-       return view('admin.tracking.create');
-    });
-});
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
