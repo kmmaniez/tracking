09284dae42a25d3a67acd7c1f6aa6f11971de32c
kmmaniez <dzlabsid@gmail.com>
2023-07-06 23:24:24 +0700

    update dashboard supir

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,7 +102,40 @@
                         </div>
                     </div>
                 </div> --}}
-                {{-- muncul paket sesuai paket yg dipilih --}}
+
+                <div class="col-lg-12">
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">List {{ $title_page ?? 'Title' }}</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table" id="productDataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>ID Checkpoint</th>
+                                            <th>Kota Tujuan</th>
+                                            <th>Checkpoint 1</th>
+                                            <th>Checkpoint 2</th>
+                                            <th>Checkpoint 3</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($checkpoints as $data)
+                                        <tr>
+                                            <td>{{ $data->id_cp }}</td>
+                                            <td>{{ $data->kota_tujuan }}</td>
+                                            <td>{{ $data->cp1 }}</td>
+                                            <td>{{ $data->cp2 }}</td>
+                                            <td>{{ $data->cp3 }}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endif
 
         </div>
