859100d386b88f291a613e0a83e6ff6091332c43
kmmaniez <dzlabsid@gmail.com>
2023-07-06 23:23:49 +0700

    update controllers

--- app/Http/Controllers/CheckpointController.php
+++ app/Http/Controllers/CheckpointController.php
@@ -15,8 +15,49 @@ class CheckpointController extends Controller
         $checkpointone = DB::table('checkpointone')->get();
         $checkpointtwo = DB::table('checkpointtwo')->get();
         $checkpointthree = DB::table('checkpointthree')->get();
-        // dump($checkpointone);
-        return view('admin.checkpoint.index', compact('title_page', 'checkpointone','checkpointtwo','checkpointthree'));
+        $dataCpone = $checkpointone->map(function($item){
+            return [
+                    'tanggal_cp_1' => $item->tanggal_cp_1,
+                    'kondisi_cp_1' => $item->kondisi_cp_1,
+                    'status_cp_1' => $item->status_cp_1,
+                    'kota_cp_1' => $item->kota_cp_1,
+            ];
+        })->toArray();
+        $dataCptwo = $checkpointtwo->map(function($item){
+            return [
+                    'tanggal_cp_2' => $item->tanggal_cp_2,
+                    'kondisi_cp_2' => $item->kondisi_cp_2,
+                    'status_cp_2' => $item->status_cp_2,
+                    'kota_cp_2' => $item->kota_cp_2,
+            ];
+        })->toArray();
+        $dataCpthree = $checkpointthree->map(function($item){
+            return [
+                    'tanggal_cp_3' => $item->tanggal_cp_3,
+                    'kondisi_cp_3' => $item->kondisi_cp_3,
+                    'status_cp_3' => $item->status_cp_3,
+                    'kota_cp_3' => $item->kota_cp_3,
+            ];
+        })->toArray();
+        $result = [];
+        $length = max(count($dataCpone), count($dataCptwo), count($dataCpthree));
+
+        for ($i = 0; $i < $length; $i++) {
+            $result[$i] = [];
+
+            if (isset($dataCpone[$i])) {
+                $result[$i] += $dataCpone[$i];
+            }
+
+            if (isset($dataCptwo[$i])) {
+                $result[$i] += $dataCptwo[$i];
+            }
+
+            if (isset($dataCpthree[$i])) {
+                $result[$i] += $dataCpthree[$i];
+            }
+        }
+        return view('admin.checkpoint.index', compact('title_page','result', 'checkpointone','checkpointtwo','checkpointthree'));
     }
     public function store_cp_1(Request $request)
     {
--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Paket;
 use App\Models\Sopir;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use PDF;
 
 class LaporanController extends Controller
@@ -25,8 +26,54 @@ class LaporanController extends Controller
     }
     
     public function laporanTracking(){
+        $checkpointone = DB::table('checkpointone')->get();
+        $checkpointtwo = DB::table('checkpointtwo')->get();
+        $checkpointthree = DB::table('checkpointthree')->get();
+        $dataCpone = $checkpointone->map(function($item){
+            return [
+                    'tanggal_cp_1' => $item->tanggal_cp_1,
+                    'kondisi_cp_1' => $item->kondisi_cp_1,
+                    'status_cp_1' => $item->status_cp_1,
+                    'kota_cp_1' => $item->kota_cp_1,
+            ];
+        })->toArray();
+        $dataCptwo = $checkpointtwo->map(function($item){
+            return [
+                    'tanggal_cp_2' => $item->tanggal_cp_2,
+                    'kondisi_cp_2' => $item->kondisi_cp_2,
+                    'status_cp_2' => $item->status_cp_2,
+                    'kota_cp_2' => $item->kota_cp_2,
+            ];
+        })->toArray();
+        $dataCpthree = $checkpointthree->map(function($item){
+            return [
+                    'tanggal_cp_3' => $item->tanggal_cp_3,
+                    'kondisi_cp_3' => $item->kondisi_cp_3,
+                    'status_cp_3' => $item->status_cp_3,
+                    'kota_cp_3' => $item->kota_cp_3,
+            ];
+        })->toArray();
+        $result = [];
+        $length = max(count($dataCpone), count($dataCptwo), count($dataCpthree));
+
+        for ($i = 0; $i < $length; $i++) {
+            $result[$i] = [];
+
+            if (isset($dataCpone[$i])) {
+                $result[$i] += $dataCpone[$i];
+            }
+
+            if (isset($dataCptwo[$i])) {
+                $result[$i] += $dataCptwo[$i];
+            }
+
+            if (isset($dataCpthree[$i])) {
+                $result[$i] += $dataCpthree[$i];
+            }
+        }
         return view('admin.laporan.laporan-tracking',[
-            'title_page' => 'Laporan Tracking'
+            'title_page' => 'Laporan Tracking',
+            'result' => $result
         ]);
     }
 
@@ -42,9 +89,14 @@ class LaporanController extends Controller
 
     }
 
-    public function printPaket()
+    public function printPaket($id)
     {
-        # code...
+        $paket = Paket::where('id',$id)->get();
+        $nama = $paket[0]->nama_customer;
+        $pdf    = PDF::loadView('admin.laporan.cetak.cetak-paket',[
+            'paket'  => $paket ,
+        ]);
+        return $pdf->download("paket-".$nama.".pdf");
     }
 
     public function printTracking()
