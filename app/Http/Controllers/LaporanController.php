a0abce7ea91ab1d4eb0ae6b1d19240137171dbf9
kmmaniez <dzlabsid@gmail.com>
2023-07-06 23:46:56 +0700

    update controller laporan

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -99,8 +99,57 @@ class LaporanController extends Controller
         return $pdf->download("paket-".$nama.".pdf");
     }
 
-    public function printTracking()
+    public function printTracking($id)
     {
-        # code...
+        $checkpointone = DB::table('checkpointone')->get();
+        $checkpointtwo = DB::table('checkpointtwo')->get();
+        $checkpointthree = DB::table('checkpointthree')->get();
+        $dataCpone = $checkpointone->map(function($item){
+            return [
+                    'tanggal_cp_1' => $item->tanggal_cp_1,
+                    'kondisi_cp_1' => $item->kondisi_cp_1,
+                    'status_cp_1' => $item->status_cp_1,
+                    'kota_cp_1' => $item->kota_cp_1,
+            ];
+        })->toArray();
+        $dataCptwo = $checkpointtwo->map(function($item){
+            return [
+                    'tanggal_cp_2' => $item->tanggal_cp_2,
+                    'kondisi_cp_2' => $item->kondisi_cp_2,
+                    'status_cp_2' => $item->status_cp_2,
+                    'kota_cp_2' => $item->kota_cp_2,
+            ];
+        })->toArray();
+        $dataCpthree = $checkpointthree->map(function($item){
+            return [
+                    'tanggal_cp_3' => $item->tanggal_cp_3,
+                    'kondisi_cp_3' => $item->kondisi_cp_3,
+                    'status_cp_3' => $item->status_cp_3,
+                    'kota_cp_3' => $item->kota_cp_3,
+            ];
+        })->toArray();
+        $result = [];
+        $length = max(count($dataCpone), count($dataCptwo), count($dataCpthree));
+
+        for ($i = 0; $i < $length; $i++) {
+            $result[$i] = [];
+
+            if (isset($dataCpone[$i])) {
+                $result[$i] += $dataCpone[$i];
+            }
+
+            if (isset($dataCptwo[$i])) {
+                $result[$i] += $dataCptwo[$i];
+            }
+
+            if (isset($dataCpthree[$i])) {
+                $result[$i] += $dataCpthree[$i];
+            }
+        }
+        
+        $pdf    = PDF::loadView('admin.laporan.cetak.cetak-tracking',[
+            'result'  => $result[$id],
+        ]);
+        return $pdf->download("Detail_tracking_".$result[$id]['kota_cp_1']."-".$result[$id]['kota_cp_2']."-".$result[$id]['kota_cp_3'].".pdf");
     }
 }
